b6ce6e5d78ffa90f274dca1937fc74b27e798675
Ivan Yu <85257356+IvanYu327@users.noreply.github.com>
2021-11-11 03:03:55 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -29,7 +29,10 @@ def login():
         if (response == "Success"):
             session["email"] = email
             session["username"] = Methods.getUsername(email)
-            return redirect(url_for("profile", user = session["username"], email = session["email"]))
+            if email == "admin":
+                return redirect(url_for("admin"))
+            else:
+                return redirect(url_for("bingo"))
         else:
             return render_template("login.html",login_fail = response)
     else:
@@ -48,26 +51,71 @@ def register():
         if response == "Success":
             session["email"] = email
             session["username"] = Methods.getUsername(email)
-            return redirect(url_for("profile"), user = session["username"], email = session["email"])
+            return redirect(url_for("bingo"))
         else:
             return render_template("register.html",signup_fail = response)
     else:
         #if no input, refresh the page with no change
         return render_template("register.html")
 
-@app.route('/bingo')
+@app.route('/bingo', methods=["POST","GET"])
 def bingo():
+    if request.method == "POST":
+        print("owo")
+        
+        # rows, cols = (5, 5)
+        userInput = [ [ None for i in range(5) ] for j in range(5) ]
+        
+        for row in range(0,5):
+            for col in range(0,5):
+                text = "bingo-field-"+str(row*5+col+1)
+                if str(request.form.getlist(text)) == "['on']":
+                    userInput[row][col]="X"
+                else:
+                    userInput[row][col]="O"
+
+        print(userInput)
+
     if "username" in session:
-        return render_template("bingo.html",user = session["username"])
+        bingo = Methods.getUserBingo(session["email"])
+        return render_template("bingo.html", user = session["username"], email = session["email"], bingos = Methods.getBingoPts(session["email"]),
+        b1 = bingo[0],
+        b2 = bingo[1],
+        b3 = bingo[2],
+        b4 = bingo[3],
+        b5 = bingo[4],
+        b6 = bingo[5],
+        b7 = bingo[6],
+        b8 = bingo[7],
+        b9 = bingo[8],
+        b10 = bingo[9],
+        b11 = bingo[10],
+        b12 = bingo[11],
+        b13 = bingo[12],
+        b14 = bingo[13],
+        b15 = bingo[14],
+        b16 = bingo[15],
+        b17 = bingo[16],
+        b18 = bingo[17],
+        b19 = bingo[18],
+        b20 = bingo[19],
+        b21 = bingo[20],
+        b22 = bingo[21],
+        b23 = bingo[22],
+        b24 = bingo[23],
+        b25 = bingo[24],)
     else:
         return redirect(url_for(""))
 
-@app.route('/profile')
-def profile():
-    if "username" in session:
-        return render_template("profile.html",user = session["username"],email = session["email"])
+@app.route('/admin')
+def admin():
+    if "username" in session and session["username"] == "admin":
+        Methods.writeMasterJSON()
+        master = [1,2]
+        return render_template("admin.html")
     else:
-        return render_template("profile.html")
+        return redirect(url_for(""))
+
 
 app.debug = True
 #Runs the app
--- methods.py
+++ methods.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
 import re
+import json
+import random
 
 
 from dotenv import load_dotenv
@@ -11,6 +13,7 @@ MONGO_KEY = getenv("MONGO")
 cluster=MongoClient(MONGO_KEY)
 database = cluster["LiquidBingo"]
 userCollection = database["Users"]
+masterCollection = database["Admin_Valorant"]
 
 regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
@@ -31,18 +34,20 @@ def signUpMethod(email,username,password):
     if (userCollection.count_documents({"_id":email}) == 0):
         if checkEmail(email) == False:
             return "Please enter a valid email"
-        elif len(username) < 4 or len(username) > 20:
-            return "Please enter a username between 4-20 characters."
-        elif len(password) < 4 or len(password) > 20:
-            return "Please enter a password between 4-20 characters."
+        elif len(username) < 3 or len(username) > 20:
+            return "Please enter a username between 3-20 characters."
+        elif len(password) < 3 or len(password) > 20:
+            return "Please enter a password between 3-20 characters."
 
         userInitPost = {
             "_id":email,
             "Username":username,
             "Password":password,
-            "Points":0
+            "BingoPts":0,
+            "Bingo":{}
         }
         userCollection.insert_one(userInitPost)
+        resetBingo(email)
         return "Success"
     else:
         return "Account already exists"
@@ -53,6 +58,14 @@ def checkEmail(email):
     else:
         return False
 
+def resetBingo(email):
+    masterPost = masterCollection.find_one({"_id":"Valorant"})
+    master = []
+    for key in masterPost["items"]:
+        master.append(key)
+    print(master)
+
+    userCollection.find_one_and_update({"_id":email},{"$set":{"Bingo":random.sample(master,25)}})
 
 def getUsername(email):
     userPost = userCollection.find_one({"_id":email})
@@ -61,3 +74,18 @@ def getUsername(email):
 def getPassword(email):
     userPost = userCollection.find_one({"_id":email})
     return userPost["Password"]
+
+def getBingoPts(email):
+    userPost = userCollection.find_one({"_id":email})
+    return userPost["BingoPts"]
+
+def getUserBingo(email):
+    userPost = userCollection.find_one({"_id":email})
+    return userPost["Bingo"]
+
+def writeMasterJSON():
+    masterPost = masterCollection.find_one({"_id":"Valorant"})
+    with open ("static/js/master.json", 'w') as f:
+        json.dump(masterPost["items"], f, indent=4)
+
+# signUpMethod("ivnyu@gmail.com","weewoo","123")
